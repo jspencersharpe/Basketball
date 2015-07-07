7f5f61da28228dc2cd25532c98b2fae4e6f39674
Spencer Sharpe <jspencersharpe@gmail.com>
2015-07-07 15:50:11 -0500

    getting id's of all teams

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 var pre = 'https://jsonp.afeld.me/?url=';
 $.get(pre + "https://api.sportradar.us/nba-t3/games/2014/reg/schedule.json?api_key=xama6vm9k7758y5fuqfkvbw6", function(data){
 
-  for (var i = 0; i < 100; i++) {
+  var array = [];
+  for (var i = 0; i < 64; i++) {
     var team = {
       id: data.games[i].home.id,
       team: data.games[i].home.name
     };
-    console.log(team);
+    //console.log(team);
+    array.push(data.games[i].home);
+    console.log(array);
+
+    .uniq(array);
   }
 });
 
-/*function getData() {
-  var id = "583ec7cd-fb46-11e1-82cb-f4ce4684ea4c";
- $.get(pre + "http://api.sportradar.us/nba-t3/teams/" + "/profile.json?api_key=xama6vm9k7758y5fuqfkvbw6", function(data){
-   console.log(data);
- })
-}*/
-
+//gets and appends Indiana Pacers
 $.get(pre + "http://api.sportradar.us/nba-t3/teams/583ec7cd-fb46-11e1-82cb-f4ce4684ea4c/profile.json?api_key=xama6vm9k7758y5fuqfkvbw6", function(data){
   var team = data.players;
 
@@ -25,3 +24,4 @@ $.get(pre + "http://api.sportradar.us/nba-t3/teams/583ec7cd-fb46-11e1-82cb-f4ce4
   }
   
 })
+
