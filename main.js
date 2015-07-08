198e3114729fe226769e32a1dbf19ead2f7852de
Spencer Sharpe <jspencersharpe@gmail.com>
2015-07-08 15:21:55 -0500

    refactor attempt

--- main.js
+++ main.js
@@ -9,40 +9,45 @@ $.ajax({
     format: 'json'
   }, 
   success: function(data){
-      var idArray = []; 
-    $.each(data.games, function(index, value){
-      var array = data.games[index].home;
-      $.each(array, function(index, value){
-        if (array[index].length === 36) {
-          var id = array[index];
-          idArray.push(id);
-        }
-      })
-    //console.log(idArray); 
-    var newArr = _.uniq(idArray);
-    console.log(newArr);
-  }) 
+    getAndSort(data);
   }, 
   error: function(err) {
-    alert("error getting data");
+    //console.log(err, "error getting data");
   }
-})
+});
 
-$.ajax({
-  url: pre + "http://api.sportradar.us/nba-t3/teams/583ec7cd-fb46-11e1-82cb-f4ce4684ea4c/profile.json?api_key=xama6vm9k7758y5fuqfkvbw6",
-  jsonp: 'callback',
-  datatype: 'jsonp',
-  data: {
-    format: 'json'
-  }, 
-  success: function(data) {
-    var team = data.players;
-
-    for (var i = 0; i < team.length; i++) {
-      $("body").append(team[i].full_name);
-    }
-  }
-})
+function getAndSort(data) {
+  var objs = [];      
+    var array = data.games;
+      array.forEach(function(item){
+        var newTeam = {
+          id: item.home.id,
+          team: item.home.name
+        }
+        objs.push(newTeam);
+      })    
+      console.log(objs);
+}
 
 
+/*for (var i = 0; i < 30; i++) {
+        $.ajax({
+          url: pre + 'https://api.sportradar.us/nba-t3/teams/' + unique[i] + '/profile.json?api_key=xama6vm9k7758y5fuqfkvbw6',
+          jsonp: 'callback',
+          datatype: 'jsonp',
+          data: {
+            format: 'json'
+          }, 
+          success: function(data) {
+            var team = data.players;
+            console.log(team);
 
+          for (var i = 0; i < team.length; i++) {
+            $("body").append(team[i].full_name);
+          }
+        }      
+        }); 
+
+      }  
+
+   */     
